3841e307aa530e2499bdbf8c490978a08ab2005d
林千里 <lincheney@gmail.com>
2017-09-10 18:43:50 +1000

    i3 icon patch https://github.com/ashinkarov/i3-extras#icons-in-i3bar

--- include/data.h
+++ include/data.h
@@ -460,6 +460,11 @@ struct Window {
 
     /* aspect ratio from WM_NORMAL_HINTS (MPlayer uses this for example) */
     double aspect_ratio;
+
+    /** Window icon, as array of ARGB pixels */
+    uint32_t* icon;
+    int icon_width;
+    int icon_height;
 };
 
 /**
